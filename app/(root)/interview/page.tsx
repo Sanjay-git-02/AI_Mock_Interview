13c1c98e446714e52548aba9a9b4a6f14ab70377
leodas221 <sanjusanjay06406@gmail.com>
2026-03-06 13:43:47 +0530

    Completed Interview generation ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Agent from "@/comps/Agent";
+
+const Page = () => {
+    return (
+        <>
+            <h3>Interview Page</h3>
+
+            <Agent userName="Leo" userId="1" type="generate"/>
+        </>
+    )
+}
+export default Page
